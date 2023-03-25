6f13617e58c34d67e7edcd13b0e55496cf5af5a0
wiktoriawawer <viktoriawawer@gmail.com>
2023-03-25 13:51:01 +0100

    poczatek projektu

---
+++ Procesor_CSS/Procesor_CSS.cpp
@@ -0,0 +1,26 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+class ListSelektors {
+
+};
+class ListAtrybuts {
+
+};
+class Block {
+    ListSelektors selectors;
+    ListAtrybuts atrybuts;
+};
+class ListSection {
+    Block Blocks[8];
+    ListSection* prev;
+    ListSection* next;
+
+};
+int main()
+{
+    cout << "Hello World!\n";
+}
+
