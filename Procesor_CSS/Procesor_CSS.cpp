f68b34a2addfa2c6e0ae50c64c786c501c95e932
wiktoriawawer <viktoriawawer@gmail.com>
2023-03-26 20:00:38 +0200

    commit

--- Procesor_CSS/Procesor_CSS.cpp
+++ Procesor_CSS/Procesor_CSS.cpp
@@ -1,26 +1,67 @@
 ﻿
-
-
+#include <stdio.h>
 #include <iostream>
 using namespace std;
-class ListSelektors {
+
+class Element {
+    char* tekst; 
+    Element* next;
+};
+class ListElements {
+    Element* firstnode;
+    void AddNext(char* tekst) {
+
+    }
+    void Delete(char* tekst) {
+
+    }
+    void Find(char* tekst) {
+
+    }
+    int GetListLen() {
+
+    }
+    Element* GetElement(int position) {
+
+    }
+    int CountElement(char* valueofelement) {
+
+    }
 
 };
-class ListAtrybuts {
 
-};
 class Block {
-    ListSelektors selectors;
-    ListAtrybuts atrybuts;
+    ListElements* selektors;
+    ListElements* atrybuts;
+        
 };
 class ListSection {
     Block Blocks[8];
+    int counter; 
     ListSection* prev;
     ListSection* next;
 
 };
+void ReadToList(char* text, char separator) {
+
+}
 int main()
-{
-    cout << "Hello World!\n";
+{   
+    bool read = true; 
+    char x;
+    char tekst[120];
+    int i = 0;
+    while (read) {
+        cin >> x;
+        if (x == '{') {
+            cout << "Koniec";
+            cout << tekst;
+
+        }
+        tekst[i] = x;//spacje i enter sie nie wczytuje !
+        i++;
+        
+    }
+
 }
 
